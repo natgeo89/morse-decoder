9d973431af58cb256beacdbf33206609d8149472
Sakharuk Viktar <natgeogr93@gmail.com>
2020-10-02 13:05:18 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arrOfCharCodes = []; 
+
+    while(true){
+        let charCode = expr.slice(0, 10);
+        arrOfCharCodes.push(charCode);        
+        expr = expr.replace(charCode, '');
+        if ( expr.length === 0 ) break;
+    }
+   
+    const arrOfDecodeChars = arrOfCharCodes.map(item =>{
+
+        if (item === '**********') return ' ';
+
+        let dotsAndDashes = '';
+        while(true){
+            if (item.slice(-2) === '10') dotsAndDashes = `.${dotsAndDashes}`;
+            
+            if (item.slice(-2) === '11') dotsAndDashes = `-${dotsAndDashes}`;             
+                        
+            item = item.slice(0, -2);
+
+            if (item.length === 0 || item.slice(-2) === '00') break;     
+        }    
+        return MORSE_TABLE[dotsAndDashes];
+    })
+
+    return arrOfDecodeChars.join('');
 }
 
 module.exports = {
